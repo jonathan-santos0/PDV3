f3e52da457774287c555562e1d9f72df4aa34f96
Jonathan Santos <j.san.costa95@gmail.com>
2024-04-05 22:28:41 -0300

    Criado campo de login, campo nome

--- PDV.app/Main.Designer.cs
+++ PDV.app/Main.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Main";
+            label1 = new Label();
+            TBName = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(219, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Bem vindo ao Venda Fácil";
+            // 
+            // TBName
+            // 
+            TBName.Location = new Point(12, 60);
+            TBName.Name = "TBName";
+            TBName.Size = new Size(116, 23);
+            TBName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 42);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Entre com seu nome";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(134, 60);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(792, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(TBName);
+            Controls.Add(label1);
+            Name = "Main";
+            Text = "PDV";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox TBName;
+        private Label label2;
+        private Button button1;
     }
 }
--- PDV.app/Main.cs
+++ PDV.app/Main.cs
@@ -16,5 +16,10 @@ namespace PDV.app
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Bem vindo {TBName.Text}");
+        }
     }
 }
